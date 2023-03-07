a80654ed908982089b5a7c9bbcaf85e9b43a8579
Yasmin Garcia Garcia <yasmin.garcia004@mymdc.net>
2023-03-07 21:26:47 +0000

    Instantiate Lab finished

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -17,7 +17,9 @@ public class Lab {
      *
      * @return any object.
      */
-    public Object instantiateANewObject(){
-        return null;
+    public Object instantiateANewObject()
+    {
+        Object animal = new Object();
+        return animal;
     }
 }
